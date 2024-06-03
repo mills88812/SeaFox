fa4b94b764671d23a3b379412aec9a1285c0c8df
Mills888dev <135458163+mills88812@users.noreply.github.com>
2024-06-02 23:11:43 -0500

    final release
    finalized UE5 looks

---
+++ ue code/Intermediate/Source/Seafox3.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class Seafox3 : ModuleRules
+{
+	public Seafox3(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
